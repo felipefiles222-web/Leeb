86f390c266a7494f43fceb51c9e21b7089cbf1c5
felipefiles222-web <felipefiles222@gmail.com>
2026-07-10 20:23:30 -0400

    feat: Complete TombRaiser hook with GraveType property for ARM64 Release

--- PvZ2/Zombie_TombRaiser.h
+++ PvZ2/Zombie_TombRaiser.h
@@ -1,17 +1,41 @@
 #pragma once
-#include "PvZ2/Zombie.h"
+#include "ZombiePropertySheet.h"
+#include "Reflection/Reflection.h"
 
-class ZombieTombRaiserProps
+class ZombieTombRaiserProps : public ZombiePropertySheet
 {
 public:
 	// Main customizable grave type property
-	int GraveType = 0; // 0 = default gravestone_egypt
+	SexyString GraveType = "gravestone_egypt";
 	
 	// Additional properties for extensibility
-	int ProjectileType = 0; // 0 = default TombraiserProjectile
+	SexyString ProjectileType = "TombraiserProjectile";
 	float GraveSpawnOffsetX = 0.0f;
 	float GraveSpawnOffsetY = 0.0f;
 	int GraveHealth = 100;
 
+	static Reflection::CRefManualSymbolBuilder::BuildSymbolsFunc oTombRaiserPropsBuildSymbols;
 	static void modInit();
+
+	static void* construct()
+	{
+		auto* props = new ZombieTombRaiserProps();
+
+		typedef void* (*ctorWithThisPtr)(void*);
+		ctorWithThisPtr baseCtor = (ctorWithThisPtr)getActualOffset(0x163A068);
+		baseCtor(props);
+
+		*reinterpret_cast<int*>(uintptr_t(props)) = getActualOffset(0x23E1128);
+		return props;
+	}
+
+	static void buildSymbols(Reflection::CRefManualSymbolBuilder* builder, Reflection::RClass* rclass)
+	{
+		oTombRaiserPropsBuildSymbols(builder, rclass);
+		REGISTER_STRING_PROPERTY(builder, rclass, ZombieTombRaiserProps, GraveType);
+		REGISTER_STRING_PROPERTY(builder, rclass, ZombieTombRaiserProps, ProjectileType);
+		REGISTER_STANDARD_PROPERTY(builder, rclass, ZombieTombRaiserProps, GraveSpawnOffsetX);
+		REGISTER_STANDARD_PROPERTY(builder, rclass, ZombieTombRaiserProps, GraveSpawnOffsetY);
+		REGISTER_STANDARD_PROPERTY(builder, rclass, ZombieTombRaiserProps, GraveHealth);
+	}
 };
